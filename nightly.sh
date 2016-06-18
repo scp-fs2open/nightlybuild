7c7c6f701482548191aa542b778985d86a24f210
asarium <asarium@gmail.com>
2016-06-18 19:18:30 +0200

    Add prove of concept nightly bash script

---
+++ nightly.sh
@@ -0,0 +1,48 @@
+#!/bin/env sh
+
+set -ex
+
+GIT_PATH=$(cat config.yml | shyaml get-value git.repo)
+GIT_BRANCH=$(cat config.yml | shyaml get-value git.branch)
+
+echo $GIT_PATH
+echo $GIT_BRANCH
+
+PREVIOUS_DIR=$(pwd)
+
+cd "$GIT_PATH"
+git checkout "$GIT_BRANCH"
+
+STASHED_CHANGES=false
+if ! git diff-index --quiet HEAD --; then
+    # Stash changes so we can recover them laster
+    echo "Stashing local changes for later recovery"
+    git stash -u -a
+    
+    STASHED_CHANGES=true
+fi
+
+COMMIT_HASH="$(git rev-parse --short HEAD)"
+CURRENT_DATE="$(date +%Y%m%d)"
+
+BUILD_NAME="${CURRENT_DATE}_$COMMIT_HASH"
+
+# Detach HEAD so we don't change the branch we are on
+git checkout --detach
+
+# We have a clean working copy, do the changes
+echo "dnl Test change" >> configure.ac
+
+git add .
+git commit -m "Automated nightly commit" --author="Nightly script <nightly@example.com>"
+git tag -a "nightly_$BUILD_NAME" -m "Nightly script tag"
+git push --tags
+
+# Go back to the actual branch
+git checkout "$GIT_BRANCH"
+
+if [ "$STASHED_CHANGES" = true ]; then
+    echo "Restoring previous changes"
+    git stash pop
+fi
+cd "$PREVIOUS_DIR"
