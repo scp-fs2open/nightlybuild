24868a4bbd6c23cdaa3793da581df128c0197590
ngld <ngld@tproxy.de>
2023-04-16 18:59:02 +0200

    Replace FTP file listing with HTTP file listing since we no longer have FTP access to the mirror we previously used and it's insecure anyway

--- test/test_ftp.py
+++ test/test_ftp.py
@@ -1,23 +1,34 @@
 import unittest
-from unittest import mock
-from unittest.mock import Mock
+import requests_mock
+
+FILE_LISTING = '''<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
+<html>
+ <head>
+  <title>Index of /builds/nightly/test_tag</title>
+ </head>
+ <body>
+<h1>Index of /builds/nightly/test_tag</h1>
+  <table>
+   <tr><th valign="top"><img src="/icons/blank.gif" alt="[ICO]"></th><th><a href="?C=N;O=D">Name</a></th><th><a href="?C=M;O=A">Last modified</a></th><th><a href="?C=S;O=A">Size</a></th><th><a href="?C=D;O=A">Description</a></th></tr>
+   <tr><th colspan="5"><hr></th></tr>
+<tr><td valign="top"><img src="/icons/back.gif" alt="[PARENTDIR]"></td><td><a href="/builds/nightly/">Parent Directory</a></td><td>&nbsp;</td><td align="right">  - </td><td>&nbsp;</td></tr>
+<tr><td valign="top"><img src="/icons/compressed.gif" alt="[   ]"></td><td><a href="nightly_test_tag-builds-Linux.tar.gz">nightly_test_tag-builds-Linux.tar.gz</a></td><td align="right">2023-04-13 06:25  </td><td align="right"> 30M</td><td>&nbsp;</td></tr>
+<tr><td valign="top"><img src="/icons/compressed.gif" alt="[   ]"></td><td><a href="nightly_test_tag-builds-MacOSX.tar.gz">nightly_test_tag-builds-MacOSX.tar.gz</a></td><td align="right">2023-04-13 06:24  </td><td align="right"> 17M</td><td>&nbsp;</td></tr>
+<tr><td valign="top"><img src="/icons/compressed.gif" alt="[   ]"></td><td><a href="nightly_test_tag-builds-Win32.zip">nightly_test_tag-builds-Win32.zip</a></td><td align="right">2023-04-13 06:43  </td><td align="right"> 29M</td><td>&nbsp;</td></tr>
+<tr><td valign="top"><img src="/icons/compressed.gif" alt="[   ]"></td><td><a href="nightly_test_tag-builds-x64.zip">nightly_test_tag-builds-x64.zip</a></td><td align="right">2023-04-13 06:44  </td><td align="right"> 35M</td><td>&nbsp;</td></tr>
+<tr><td valign="top"><img src="/icons/unknown.gif" alt="[   ]"></td><td><a href="nightly_test_tag-debug-Win32.7z">nightly_test_tag-debug-Win32.7z</a></td><td align="right">2023-04-13 06:43  </td><td align="right"> 68M</td><td>&nbsp;</td></tr>
+<tr><td valign="top"><img src="/icons/unknown.gif" alt="[   ]"></td><td><a href="nightly_test_tag-debug-x64.7z">nightly_test_tag-debug-x64.7z</a></td><td align="right">2023-04-13 06:44  </td><td align="right"> 71M</td><td>&nbsp;</td></tr>
+   <tr><th colspan="5"><hr></th></tr>
+</table>
+<address>Apache/2.4.25 (Debian) Server at datacorder.porphyrion.feralhosting.com Port 80</address>
+</body></html>
+'''
 
 class FtpTestCase(unittest.TestCase):
-    @mock.patch("ftplib.FTP", autospec=True)
-    def test_file_listing(self, MockFTP):
-        MockFTP.return_value = Mock()
-        mock_ftp_obj = MockFTP()
-        mock_ftp_obj.__enter__ = Mock(return_value=mock_ftp_obj)
-        mock_ftp_obj.__exit__ = Mock()
+    @requests_mock.mock()
+    def test_file_listing(self, m):
+        m.get("http://01.example.org/builds/nightly/test_tag/", text=FILE_LISTING)
 
-        mock_ftp_obj.mlsd = Mock(return_value=[
-            (".", {"type": "cdir"}),
-            ("..", {"type": "pdir"}),
-            ("nightly_test_tag-builds-Linux.tar.gz", {"type": "file"}),
-            ("nightly_test_tag-builds-MacOSX.tar.gz", {"type": "file"}),
-            ("nightly_test_tag-builds-Win32.zip", {"type": "file"}),
-            ("nightly_test_tag-builds-x64.zip", {"type": "file"}),
-        ])
         test_config = {
             "ftp": {
                 "host": "example.org",
