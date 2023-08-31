68b267d5f496cb8a995ff15c54fa97d064120626
Goober5000 <ipw47@mailbox.org>
2023-08-30 23:35:33 -0400

    Remove travis and appveyor

--- build_monitor/monitors.py
+++ build_monitor/monitors.py
@@ -1,6 +1,5 @@
 import github
 import requests
-import travispy
 from github import Github
 
 from util import GLOBAL_TIMEOUT
@@ -35,91 +34,6 @@ class Monitor:
     @property
     def name(self):
         raise NotImplementedError()
-
-
-class TravisMonitor(Monitor):
-    def __init__(self, config, tag_name):
-        super().__init__(config, tag_name)
-        self.travis = travispy.TravisPy()
-        self.branch = None
-
-    def update_state(self):
-        self.branch = self.travis.branch(self.tag_name, self.config["travis"]["slug"])
-
-    @property
-    def running(self):
-        return (self.branch.pending or self.branch.queued or self.branch.started or self.branch.running) and not (
-                self.success or self.errored)
-
-    @property
-    def success(self):
-        return self.branch.passed
-
-    @property
-    def errored(self):
-        return self.branch.errored or self.branch.failed
-
-    @property
-    def state(self):
-        return self.branch.state
-
-    @property
-    def name(self):
-        return "Travis CI"
-
-
-class AppveyorMonitor(Monitor):
-    STATUS_QUEUED = "queued"
-    STATUS_STARTING = "starting"
-    STATUS_RUNNING = "running"
-    STATUS_SUCCESS = "success"
-
-    RUNNING_STATES = [STATUS_QUEUED, STATUS_STARTING, STATUS_RUNNING]
-    SUCCESS_STATES = [STATUS_SUCCESS]
-
-    def __init__(self, config, tag_name):
-        super().__init__(config, tag_name)
-        self.status = None
-
-    def execute_request(self, path):
-        headers = {
-            "Authorization": "Bearer " + self.config["appveyor"]["token"],
-            "Content-type": "application/json"
-        }
-        url = "https://ci.appveyor.com/api" + path
-
-        response = requests.get(url, headers=headers, timeout=GLOBAL_TIMEOUT)
-
-        response.raise_for_status()
-
-        return response.json()
-
-    def update_state(self):
-        response = self.execute_request(
-            "/projects/{}/{}/branch/{}".format(self.config["appveyor"]["user"], self.config["appveyor"]["repo"],
-                                               self.tag_name))
-
-        self.status = response["build"]["status"]
-
-    @property
-    def running(self):
-        return self.status in AppveyorMonitor.RUNNING_STATES
-
-    @property
-    def errored(self):
-        return self.status not in AppveyorMonitor.RUNNING_STATES and self.status not in AppveyorMonitor.SUCCESS_STATES
-
-    @property
-    def success(self):
-        return self.status in AppveyorMonitor.SUCCESS_STATES
-
-    @property
-    def state(self):
-        return self.status
-
-    @property
-    def name(self):
-        return "Appveyor"
 
 
 class GitHubMonitor(Monitor):
