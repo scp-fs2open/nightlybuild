12b02a355f00f8c6f1583d7ab4cbd723e18a0d26
Goober5000 <ipw47@mailbox.org>
2024-08-29 20:24:35 -0400

    nightly for 20240808

--- git.py
+++ git.py
@@ -6,7 +6,7 @@ import sys
 
 class GitRepository:
     def __init__(self, path, branch):
-        self.branch = "35db8e31c9b3e6faac5ad7ebcd9d3aedd0e508ab"
+        self.branch = "f2bffd323423589d33d9fa935f437df909b98581"
         self.path = path
 
     def _format_git_cmd(self, cmd):
--- nightly.py
+++ nightly.py
@@ -62,7 +62,7 @@ with open(args.config, "r") as f:
 class NightlyState(ScriptState):
     def __init__(self):
         super().__init__(config)
-        self.branch = "35db8e31c9b3e6faac5ad7ebcd9d3aedd0e508ab"
+        self.branch = "f2bffd323423589d33d9fa935f437df909b98581"
 
     def post_build_actions(self):
         # Get the file list
@@ -75,7 +75,7 @@ class NightlyState(ScriptState):
         version = get_source_version(self.config, self.date.strftime(ScriptState.DATEFORMAT_VERSION))
         nebula.submit_release(nebula.render_nebula_release(version, "nightly", files, config), config)
 
-        commit = "35db8e31c9b3e6faac5ad7ebcd9d3aedd0e508ab"
+        commit = "f2bffd323423589d33d9fa935f437df909b98581"
         date = self.date.strftime(ScriptState.DATEFORMAT_FORUM)
         log = self.repo.get_log("nightly_*", self.tag_name)
 
@@ -85,7 +85,7 @@ class NightlyState(ScriptState):
         return True
 
     def get_tag_name(self, params):
-        return "nightly_20240730_35db8e31c"
+        return "nightly_20240808_f2bffd323"
 
     def get_tag_pattern(self):
         return "nightly_*"
